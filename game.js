da5fbf499099f4491b29588ae41e0266ed44b83d
Jannie87 <jannie.backman@gmail.com>
2021-11-13 15:12:15 +0100

    prompt and alert added

---
+++ game.js
@@ -0,0 +1,14 @@
+ function myGame () { 
+    const person = prompt ("please enter your name");
+    alert("Hi " + person);
+
+    const scopeOne= prompt ("Enter flavor for scope 1");
+    alert("you added " + scopeOne + " to your cone");
+
+
+    const scopeTwo= prompt ("please enter another flavor");
+    alert("now you added " + scopeOne + " and " + scopeTwo + " to your cone");
+
+
+
+ }
