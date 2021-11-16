43e35d6d2c96428d8531aeda651204766b78a7b5
Jannie87 <jannie.backman@gmail.com>
2021-11-16 09:50:06 +0100

    added scenes

--- game.js
+++ game.js
@@ -1,10 +1,38 @@
+ window.onload();
+ const scene = {
+    text: '',
+    nextScene: [1, 2, 3],
+    items: ['bägare', 'rån', 'våffla']
+ }
+ const scenes = [{
+   text: 'Scene 0',
+   nextScene: [1, 2],
+   items: ['bägare', 'rån', 'våffla']
+}, {
+   text: 'Scene 1',
+   nextScene: [1, 2],
+   items: ['strössel', 'kolasås']
+}];
+
+
+ 
  function myGame () { 
-    const person = prompt ("Please enter your name");
-    alert("Hi " + person);
+    const person = prompt ("Skriv in ditt namn");
+    username = person;
+    startStory();
+
+ }
+
+ function startStory() {
+    alert("Hej " + username + ". Du är nu på väg in i ett glassigt äventyr där du skall hitta den magiska guldglassen.");
+    const answer = prompt
 
     const scopeOne= prompt ("Enter flavor for scope 1");
     alert("you added " + scopeOne + " to your cone");
    
+}
+
+
 
     const scopeTwo= prompt ("Please enter another flavor");
     alert("now you added " + scopeOne + " and " + scopeTwo + " to your cone");
@@ -18,4 +46,4 @@
     const goodBye = "Enjoy!"
     alert (goodBye)
 
- }
+ 
